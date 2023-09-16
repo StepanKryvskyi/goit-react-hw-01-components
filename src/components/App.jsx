cf37a29e037d774b6b2853c991edde9329a8aede
StepanKryvskyi <126700161+StepanKryvskyi@users.noreply.github.com>
2023-09-16 18:25:48 +0300

    update hw-react-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from './Profile/Profile';
+import user from './Data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from './Data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
