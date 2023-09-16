30c52f20236a9c7fa2e60e8e8be3748da5275e53
StepanKryvskyi <126700161+StepanKryvskyi@users.noreply.github.com>
2023-09-16 19:16:42 +0300

    finish hw-react-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import { Profile } from './Profile/Profile';
 import user from './Data/user.json';
+
 import { Statistics } from './Statistics/Statistics';
 import data from './Data/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from './Data/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './Data/transactions.json'
 
 export const App = () => {
   return (
@@ -13,7 +20,12 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
       <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
